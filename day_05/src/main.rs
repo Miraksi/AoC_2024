77132f575bd8d4bbff91515a131527889a4a226e
Miraksi <arne.prinzler@gmail.com>
2024-12-06 16:12:08 +0100

    done day 5 part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     let mut rules: HashMap<u32, HashSet<u32>> = HashMap::new();
     for line in input[0].lines() {
         let values: Vec<&str> = line.split('|').collect();
-        let x = values[0].parse::<u32>().expect("couldnt convert string to ");
-        let y = values[1].parse::<u32>().expect("couldnt convert string to ");
+        let x = values[0].parse::<u32>().expect("couldnt convert string to u32");
+        let y = values[1].parse::<u32>().expect("couldnt convert string to u32");
 
         match rules.get_mut(&x) {
             Some(set) => {set.insert(y);},
@@ -28,6 +28,7 @@ fn main() {
         ).collect();
 
     part_one(&rules, &updates);
+    part_two(&rules, &updates);
 }
 
 fn part_one(rules: &HashMap<u32,HashSet<u32>>, updates: &Vec<Vec<u32>>) {
@@ -51,4 +52,49 @@ fn is_valid(rules: &HashMap<u32,HashSet<u32>>, update: &Vec<u32>) -> bool {
         }
     }
     true
+}
+
+fn part_two(rules: &HashMap<u32,HashSet<u32>>, updates: &Vec<Vec<u32>>) {
+    let mut sum = 0;
+    for update in updates.iter() {
+        if is_valid(rules, update) {
+            continue;
+        }
+        let sorted = sort_with_rules(rules, update);
+        sum += sorted[sorted.len()/2];
+    }
+
+    println!("part two: {sum}");
+}
+
+fn sort_with_rules(rules: &HashMap<u32,HashSet<u32>>, update: &Vec<u32>) -> Vec<u32> {
+    let mut rest: HashSet<u32> = update.clone().into_iter().collect();
+    let mut sorted = Vec::new();
+
+    while rest.len() > 0 {
+        let mut min = None;
+        for key in rest.iter() {
+            if count_dependencies(rules, &rest, *key) == 0 {
+                min = Some(*key);
+            }
+        }
+        match min {
+            Some(key) => {
+                sorted.push(key);
+                rest.remove(&key);
+            },
+            None => panic!("no minimal key found"),
+        };
+    }
+    sorted
+}
+
+fn count_dependencies(rules: &HashMap<u32,HashSet<u32>>, rest: &HashSet<u32>, key: u32) -> u32 {
+    let mut count = 0;
+    for x in rest.iter() {
+        if rules[&key].contains(x) {
+            count += 1;
+        }
+    }
+    count
 }
