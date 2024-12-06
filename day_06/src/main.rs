8522892c4ba8f4c388ed5b7219e0356aa343ea6e
Miraksi <arne.prinzler@gmail.com>
2024-12-06 18:08:06 +0100

    done day 6 part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{BufReader, Read};
 use std::fs::File;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let file = File::open("input.txt").expect("File couldnt be opened");
@@ -8,17 +9,18 @@ fn main() {
     let mut tmp = String::new();
     reader.read_to_string(&mut tmp).expect("couldnt read to string");
 
-    let input: Vec<Vec<char>> = tmp.lines().map(|line| line.chars().collect()).collect();    
+    let mut input: Vec<Vec<char>> = tmp.lines().map(|line| line.chars().collect()).collect();    
 
     part_one(&input);
+    part_two(&mut input);
 }
 
 fn part_one(input: &Vec<Vec<char>>) {
-    let x_border = input[0].len() as i32 - 1;
-    let y_border = input.len() as i32 - 1;
+    let x_border = input.len() as i32 - 1;
+    let y_border = input[0].len() as i32 - 1;
     let mut pos = get_starting_pos(&input).expect("no starting position found");
     let mut dir = (-1, 0);
-    let mut visited = vec![vec![false; x_border as usize + 1]; y_border as usize + 1];
+    let mut visited = vec![vec![false; y_border as usize + 1]; x_border as usize + 1];
     let mut count = 0;
 
     while in_bounds(x_border, y_border, pos) {
@@ -29,6 +31,39 @@ fn part_one(input: &Vec<Vec<char>>) {
         make_move(input, &mut pos, &mut dir);
     }
     println!("part one: {count}");
+}
+
+fn part_two(input: &mut Vec<Vec<char>>) {
+    let x_border = input.len() as i32 - 1;
+    let y_border = input[0].len() as i32 - 1;
+    let starting_pos = get_starting_pos(&input).expect("no starting position found");
+    let mut count = 0;
+
+    for i in 0..=(x_border as usize) {
+        for j in 0..=(y_border as usize) {
+            if i as i32 == starting_pos.0 && j as i32 == starting_pos.1 {
+                continue;
+            }
+            if input[i][j] == '#' {
+                continue;
+            }
+            let mut pos = starting_pos.clone();
+            input[i][j] = '#';
+            let mut dir = (-1, 0);
+            let mut visited: Vec<Vec<HashSet<(i32, i32)>>> = vec![vec![HashSet::new(); y_border as usize + 1]; x_border as usize + 1];
+
+            while in_bounds(x_border, y_border, pos) {
+                if visited[pos.0 as usize][pos.1 as usize].contains(&dir) {
+                    count += 1;
+                    break;
+                }
+                make_move_2(input, &mut pos, &mut dir, &mut visited);
+            }
+
+            input[i][j] = '.';
+        }
+    }
+    println!("part two: {count}");
 }
 
 fn get_starting_pos(input: &Vec<Vec<char>>) -> Option<(i32, i32)> {
@@ -67,5 +102,25 @@ fn make_move(input: &Vec<Vec<char>>, pos: &mut (i32, i32), dir: &mut (i32, i32))
         *dir = (dir.1, dir.0 * (-1));
         next_pos = (pos.0 + dir.0, pos.1 + dir.1);
     }
+    *pos = next_pos;
+}
+
+fn make_move_2(input: &Vec<Vec<char>>, pos: &mut (i32, i32), dir: &mut (i32, i32), visited: &mut Vec<Vec<HashSet<(i32, i32)>>>) {
+    visited[pos.0 as usize][pos.1 as usize].insert(dir.clone());
+    let x_border = input[0].len() as i32 - 1;
+    let y_border = input.len() as i32 - 1;
+    let mut next_pos = (pos.0 + dir.0, pos.1 + dir.1);
+
+    if !in_bounds(x_border, y_border, next_pos) {
+        *pos = next_pos;
+        return;
+    }
+
+    while input[next_pos.0 as usize][next_pos.1 as usize] == '#' {
+        // Rotation
+        *dir = (dir.1, dir.0 * (-1));
+        next_pos = (pos.0 + dir.0, pos.1 + dir.1);
+    }
+    visited[pos.0 as usize][pos.1 as usize].insert(dir.clone());
     *pos = next_pos;
 }
