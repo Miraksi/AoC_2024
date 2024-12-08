be3a4b0f686e38258f69bef63a3e42cd9d5c978c
Miraksi <arne.prinzler@gmail.com>
2024-12-08 18:40:17 +0100

    done day 7 part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -2,19 +2,19 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 
 struct Query {
-    result: u64,
-    numbers: Vec<u64>,
+    result: u128,
+    numbers: Vec<u128>,
 }
 impl Query {
     fn new(line: &str) -> Self {
         let input: Vec<&str> = line.split(": ").collect();
         Self {
             result: input[0].parse().unwrap(),
-            numbers: input[1].split(' ').map(|x| x.parse::<u64>().expect("input couldnt be parsed")).collect(),
+            numbers: input[1].split(' ').map(|x| x.parse::<u128>().expect("input couldnt be parsed")).collect(),
         }
     }
 
-    fn solvable_plus_mul(&self, index: usize, result: u64) -> bool {
+    fn solvable_plus_mul(&self, index: usize, result: u128) -> bool {
         if result > self.result {
             return false;
         }
@@ -23,6 +23,22 @@ impl Query {
         }
 
         self.solvable_plus_mul(index + 1, result * self.numbers[index]) || self.solvable_plus_mul(index + 1, result + self.numbers[index])
+    }
+
+    fn solvable_advanced(&self, index: usize, result: u128) -> bool {
+        if result > self.result {
+            return false;
+        }
+        if index >= self.numbers.len() {
+            return result == self.result;
+        }
+        
+        let concat = (result.to_string() + &self.numbers[index].to_string()).parse().expect("faulty int concat");
+        // println!("{} || {} = {}", result, self.numbers[index], concat);
+
+        self.solvable_advanced(index + 1, result * self.numbers[index]) || 
+        self.solvable_advanced(index + 1, result + self.numbers[index]) ||
+        self.solvable_advanced(index + 1, concat)
     }
 }
 
@@ -36,6 +52,7 @@ fn main() {
     }
 
     part_one(&queries);
+    part_two(&queries);
 }
 
 fn part_one(queries: &Vec<Query>) {
@@ -47,4 +64,15 @@ fn part_one(queries: &Vec<Query>) {
         }
     }
     println!("part one: {sum}");
+}
+
+fn part_two(queries: &Vec<Query>) {
+    let mut sum = 0;
+
+    for query in queries {
+        if query.solvable_advanced(1, query.numbers[0]) {
+            sum +=query.result;
+        }
+    }
+    println!("part two: {sum}");
 }
