50c017d4f27e3fa03c484ef5d4b0bb173e372a00
arne.prinzler <arne.prinzler@stud.uni-goettingen.de>
2024-12-11 16:28:16 +0100

    done day 8 part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -20,6 +20,7 @@ fn main() {
     }
 
     part_one(&antennas_map, input[0].len() as i32, input.len() as i32);
+    part_two(&antennas_map, input[0].len() as i32, input.len() as i32);
 }
 
 fn part_one(antennas_map: &HashMap<char,Vec<(i32,i32)>>, x_border: i32, y_border: i32) {
@@ -41,6 +42,38 @@ fn part_one(antennas_map: &HashMap<char,Vec<(i32,i32)>>, x_border: i32, y_border
     }
 
     println!("part_one: {count}");
+}
+
+fn part_two(antennas_map: &HashMap<char,Vec<(i32,i32)>>, x_border: i32, y_border: i32) {
+    let mut antinodes = HashSet::new();
+    for (_, antennas) in antennas_map.iter() {
+        for combination in antennas.iter().combinations(2) {
+            let fst = combination[0];
+            let snd = combination[1];
+
+            let one_direction = (fst.0 - snd.0, fst.1 - snd.1); // my mind's tellin me noooo
+            let mut current_antinode = *snd; 
+            while in_bounds(&current_antinode, x_border, y_border) {
+                antinodes.insert(current_antinode);
+                current_antinode = (current_antinode.0 + one_direction.0, current_antinode.1 + one_direction.1);
+            }
+
+            let two_direction = (snd.0 - fst.0, snd.1 - fst.1); // BUT MY BOOOODDDYYYY
+            current_antinode = *fst;
+            while in_bounds(&current_antinode, x_border, y_border) {
+                antinodes.insert(current_antinode);
+                current_antinode = (current_antinode.0 + two_direction.0, current_antinode.1 + two_direction.1);
+            }
+        }
+    }
+
+    let mut count = 0;
+    for antinode in antinodes.iter() {
+        if in_bounds(antinode, x_border, y_border) {
+            count += 1;
+        }
+    }
+    println!("part_two: {count}");
 }
 
 fn in_bounds(antinode: &(i32,i32), x_border: i32, y_border: i32) -> bool {
