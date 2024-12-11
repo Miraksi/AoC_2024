0b9859d63d4e9e8497c6b7aa89a658fe4bb04339
Miraksi <arne.prinzler@gmail.com>
2024-12-11 19:59:02 +0100

    done day 9 part 2 (disgusting implementation)

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -22,7 +22,8 @@ fn main() {
         memory_map.append(&mut vec![file; amount as usize]);
     }
 
-    part_one(memory_map.clone());
+    // part_one(memory_map.clone());
+    part_two(memory_map.clone());
 }
 
 fn part_one(mut memory_map: Vec<Option<u128>>) {
@@ -43,5 +44,60 @@ fn part_one(mut memory_map: Vec<Option<u128>>) {
         sum += index * x;
         index += 1; 
     }
-    println!("{:?}", sum);
+    println!("part one: {}", sum);
+}
+
+fn part_two(mut memory_map: Vec<Option<u128>>) {
+    let mut end = memory_map.len() - 1;
+
+    while end >= 1 {
+
+        if memory_map[end].is_none() {
+            end -= 1;
+            continue;
+        }
+        let file = memory_map[end].unwrap();
+        let start = memory_map.iter().position(
+            |&x| match x {
+                Some(index) => index == file,
+                None => false,
+            } 
+        ).unwrap();
+        // println!("{}: {}, {}", file, start, end);
+        let file_size = (end - start) + 1;
+        if let Some(mem_start) = find_memory(&memory_map, file_size) {
+            if mem_start < end {
+                for space in memory_map[mem_start..mem_start+file_size].iter_mut() {
+                    *space = Some(file);
+                }
+                for file in memory_map[start..=end].iter_mut() {
+                    *file = None;
+                }
+            }
+        }
+        if start == 0 {
+            break;
+        }
+        end = start - 1;
+    }
+    let mut sum = 0;
+    for (i, &file) in memory_map.iter().enumerate() {
+        if let Some(x) = file {
+            sum += i as u128 * x;
+        }
+    }
+    // println!("{:?}", memory_map);
+    println!("part two: {}", sum);
+}
+
+fn find_memory(memory_map: &Vec<Option<u128>>, size: usize) -> Option<usize> {
+    'outer: for (index, window) in memory_map.windows(size).enumerate() {
+        for x in window.iter() {
+            if x.is_some() {
+                continue 'outer;
+            }
+        }
+        return Some(index);
+    }
+    None
 }
